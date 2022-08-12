8381605183bd62507363b694ca8b28d4a9a164a6
yingko123 <yingko123@yahoo.com>
2022-08-12 11:06:35 -0500

    initial commit

---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+function buildTable(data){
+    tbody.html("");
+    data.forEach((dataRow) => {
+        let row = tbody.append("tr");
+        Object.values(dataRow).forEach((val) =>{
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+};
