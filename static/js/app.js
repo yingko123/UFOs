531000b6160df654a6a2c1072b1b450c4b084c09
yingko123 <yingko123@yahoo.com>
2022-08-12 11:33:28 -0500

    update

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,7 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
+// Function that build a table with data in variable "data"
 function buildTable(data){
     tbody.html("");
     data.forEach((dataRow) => {
@@ -14,3 +15,19 @@ function buildTable(data){
         });
     });
 };
+
+// show data in a table based on user selected date range
+function handleClick () {
+    let date = d3.select("#datatime").property("value");
+    let filterData = tableData;
+    if (date) {
+        filterData = filterData.filter(row => row.datetime === date);
+    };
+    buildTable(filterData);
+};
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
